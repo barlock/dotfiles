d009a270cabec6464baec1778c50c9b6b7f7b228
Michael Barlock <barlockm@gmail.com>
2025-02-07 14:50:27 -0500

    fix install

--- install.sh
+++ install.sh
@@ -4,3 +4,12 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || true
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" || true
+
+rsync --exclude ".git/" \
+  --exclude ".DS_Store" \
+  --exclude ".osx" \
+  --exclude "bootstrap.sh" \
+  --exclude "install.sh" \
+  --exclude "brew.sh" \
+  --exclude "README.md" \
+  -avh --no-perms . ~;
